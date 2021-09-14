d0c5af7777cb9d69932a63b5e416a58cc00ae83d
Javaughn Bailey <javaughnbailey21@gmail.com>
2021-09-14 15:38:02 -0500

    Migrations And Env Configured

--- database/migrations/2021_09_14_194034_create_tour_companies_table.php
+++ database/migrations/2021_09_14_194034_create_tour_companies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateTourCompaniesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('tour_companies',function (Blueprint $table) {
+            $table->id("tour_company_id");
+            $table->string("company_name");
+            $table->string("credit_amt");
+            $table->timestamps();
         });
     }
 
@@ -27,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('tour_companies');
     }
 }
